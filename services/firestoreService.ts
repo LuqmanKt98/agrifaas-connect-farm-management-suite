adc96a9dbfe795798a6f47fb4b4325cf3bd11628
Luqmankt98 <luqmankt98@gmail.com>
2025-11-22 05:37:37 +0500

    fix: Add missing addDocument function to firestoreService

--- services/firestoreService.ts
+++ services/firestoreService.ts
@@ -1,20 +1,23 @@
 import { db } from '../config/firebase';
-import { collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, query, where } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, query, where, addDoc } from 'firebase/firestore';
 
-export const getDocument = (collectionName: string, docId: string) => 
+export const getDocument = (collectionName: string, docId: string) =>
   getDoc(doc(db, collectionName, docId));
 
-export const getCollection = (collectionName: string) => 
+export const getCollection = (collectionName: string) =>
   getDocs(collection(db, collectionName));
 
-export const queryCollection = (collectionName: string, field: string, operator: any, value: any) => 
+export const queryCollection = (collectionName: string, field: string, operator: any, value: any) =>
   getDocs(query(collection(db, collectionName), where(field, operator, value)));
 
-export const setDocument = (collectionName: string, docId: string, data: any) => 
+export const setDocument = (collectionName: string, docId: string, data: any) =>
   setDoc(doc(db, collectionName, docId), data);
 
-export const updateDocument = (collectionName: string, docId: string, data: any) => 
+export const addDocument = (collectionName: string, data: any) =>
+  addDoc(collection(db, collectionName), data);
+
+export const updateDocument = (collectionName: string, docId: string, data: any) =>
   updateDoc(doc(db, collectionName, docId), data);
 
-export const deleteDocument = (collectionName: string, docId: string) => 
+export const deleteDocument = (collectionName: string, docId: string) =>
   deleteDoc(doc(db, collectionName, docId));
